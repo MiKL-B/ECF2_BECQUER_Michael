adeb40d23c241f2cc5fc0aebc0b4ffb2a73cd0ef
michael <michaelbecquer@yahoo.fr>
2022-04-05 11:25:56 +0200

    moyenne charge

--- request/script_sect_charge.sql
+++ request/script_sect_charge.sql
@@ -1,3 +1,3 @@
 --Requête 1 On souhaite obtenir par secteur d’activité la moyenne des charges estimées des projets.
- select libelle_court, nom_secteur_activite,charge_estimee_globale from projet 
- inner join secteur_activite on projet.id_secteur_activite = secteur_activite.id_secteur_activite;
+
+ select AVG(charge_estimee_globale) from projet  inner join secteur_activite on projet.id_secteur_activite = secteur_activite.id_secteur_activite ;
